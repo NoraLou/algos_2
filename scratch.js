4e96d7967d4665829896febe4ab57e7f7dd13dc3
Nora Humpage <nora.humpage@wu.com>
2016-07-28 15:46:10 -0700

    easy one

--- scratch.js
+++ scratch.js
@@ -1,15 +1,1 @@
-//Question: How would you make this work?
-
-add(2, 5); // 7
-add(2)(5); // 7
-
-
-function add(x) {
-    return function (y) {
-        return x + y
-    }
-}
-
-console.log(add(2)(5))
-
 
